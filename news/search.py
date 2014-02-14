7c683a064d06a154bad6807392f5c48ebaee2247
Morgante Pell <morgante.pell@morgante.net>
2014-02-15 00:52:44 +0400

    stubbing for news/search.py

---
+++ news/search.py
@@ -0,0 +1,23 @@
+
+def search(
+	query=None,
+	filter=None,
+	begin="20080101",
+	end="20081231",
+	page=None,
+	pages=10,
+	limit=100,
+	sort=None,
+	fields=["web_url", "headline", "keywords"],
+	highlight=False,
+	facet_field="source",
+	facet_filter=False
+	):
+		
+		return []; # Actually return stories here
+
+
+if __name__ == "__main__":
+	# Do testing in here
+	print 'hello'
+	print search()
