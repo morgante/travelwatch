d2340d2af8d53e0c5d9b9f9e0bbf08f9bd449968
Morgante Pell <morgante.pell@morgante.net>
2014-02-15 00:36:47 +0400

    experimenting with scikit-learn, wow this is easy & awesome

---
+++ ml/train.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn import linear_model
+from sklearn.externals import joblib
+
+data = np.loadtxt(open("train.csv","rb"),delimiter=",",skiprows=0)
+
+features = data[:, 1:-1]
+outputs = data[:, -1].astype(np.int)
+
+# clf = ExtraTreesClassifier(n_estimators=100).fit(features[:1], outputs[:1])
+
+model = linear_model.LinearRegression()
+
+train_ft = features[:-5]
+train_out = outputs[:-5]
+test_ft = features[-5:]
+test_out = outputs[-5:]
+
+model.fit(train_ft, train_out)
+
+print('Variance score: %.2f' % model.score(test_ft, test_out))
+
+# make prediction on test data
+print('Prediction:')
+print(model.predict(test_ft))
+
+print('Actual:')
+print test_out
+
+# Save to disk
+filename = 'model/store.pkl'
+joblib.dump(model, filename)
+
+print('Saved model to ' + filename)
