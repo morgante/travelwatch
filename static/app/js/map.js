2026d2cf7e422f342abbf220056dbfb1c597c620
Zack Newman <znewman01@gmail.com>
2014-02-15 17:53:24 -0500

    Plot points on zoomed-in view

--- static/app/js/map.js
+++ static/app/js/map.js
@@ -5,6 +5,22 @@ define([
 	'topojson',
 ], function ($, _, d3, topojson) {
 
+	function translate(coords, dists) {
+		return [coords[0] + dists[0], coords[1] + dists[1]];
+	}
+
+	function scale(coords, k) {
+		return [coords[0] * k, coords[1] * k];
+	}
+
+	function polygon(d) {
+		return "M" + d.join("L") + "Z";
+	}
+
+	function dangerLevelToColor(dangerLevel) {
+		return d3.interpolateRgb('green', 'red')(dangerLevel / 100);
+	}
+
 	/**
 	 * Makes a world map from given data
 	 *
@@ -22,11 +38,11 @@ define([
 		this.svg = d3.select($el.get(0)).append("svg")
 			.attr("width", this.width)
 			.attr("height", this.height);
-		var projection = d3.geo.equirectangular()
+		this.projection = d3.geo.equirectangular()
 			.scale((this.width + 1) / 2 / Math.PI)
 			.translate([this.width / 2, this.height / 1.8]);
 		this.path = d3.geo.path()
-			.projection(projection);
+			.projection(this.projection);
 
 		d3.json("/static/app/world.topo.json", function(error, data) {
 			var subunits = that.svg.insert('g', ':first-child')
@@ -67,7 +83,7 @@ define([
 
 		d3.selectAll('.datamaps-subunit').style('fill', function(d) {
 			if (data.hasOwnProperty(d.id)) {
-				return d3.interpolateRgb('green', 'red')(data[d.id] / 100);
+				return dangerLevelToColor(data[d.id]);
 			}
 			return '#BBBBBB';
         });
@@ -88,7 +104,28 @@ define([
 	 * @param  {Function} callback A function(err) to be called on completion of colorization
 	 */
 	Map.prototype.colorPoints = function(data, callback) {
+		var that = this;
 		var error = null;
+
+		var g = this.svg.append("g")
+			.attr("id", "pointsLayer");
+		var filter = this.svg.append("defs")
+			.append("filter")
+			.attr("id", "blur")
+			.append("feGaussianBlur")
+			.attr("stdDeviation", 2);
+
+		_.each(data, function(d) {
+			var latLong = [d.position.longitude, d.position.latitude];
+			var coords = that.transform(that.projection(latLong));
+			g.append("ellipse")
+				.attr("cx", coords[0])
+				.attr("cy", coords[1])
+				.attr("rx", d.force * that.k / 50)
+				.attr("ry", d.force * that.k / 50)
+				.style("fill", dangerLevelToColor(d.score))
+				.attr("filter", 'url(#blur)');
+		});
 
 		if (callback !== undefined) {
 			callback(error);
@@ -103,9 +140,11 @@ define([
 	 * @param  {Function} callback A callback to fire once the zooming is complete, callback(err)
 	 */
 	Map.prototype.zoom = function(country, bounds, callback) {
+		var that = this;
 		var error = null;
 
-		var g = this.svg.select('g');
+		d3.selectAll("g#pointsLayer").data([]).exit().remove();
+		var g = this.svg.select('.datamaps-subunits');
 		var centroid = this.path.centroid(d3.select('.' + country).data()[0]);
 		var countryBox = this.path.bounds(d3.select('.' + country).data()[0]);
 		var x = centroid[0],
@@ -115,12 +154,17 @@ define([
 		var k = Math.min(bounds.length / countryWidth, bounds.height / countryHeight);
 		x += ((this.width - bounds.length) / 2 - bounds.x)/ k;
 
+		this.transformStr = "translate(" + this.width / 2 + "," + this.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")";
+		this.k = k;
+		this.transform = function(coords) {
+			return translate(scale(translate(coords, [-x, -y]), k), [this.width / 2, this.height / 2]);
+		};
+
 		g.selectAll("path").classed("active", true);
 
 		g.transition()
 			.duration(1000)
-			.attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-			.style("stroke-width", 1.5 / k + "px")
+			.attr("transform", that.transformStr)
 			.each("end", function() {
 				if (callback !== undefined) {
 					callback(error);
@@ -144,7 +188,8 @@ define([
 	Map.prototype.unzoom = function(callback) {
 		var error = null;
 
-		var g = this.svg.select('g');
+		d3.selectAll("g#pointsLayer").data([]).exit().remove();
+		var g = this.svg.select('.datamaps-subunits');
 		var x = this.width / 2;
 		var y = this.height / 2;
 		var k = 1;
@@ -154,7 +199,6 @@ define([
 		g.transition()
 			.duration(1000)
 			.attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-			.style("stroke-width", 1.5 / k + "px")
 			.each("end", function() {
 				if (callback !== undefined) {
 					callback(error);
