f2e8d9a422c15f16211b8e6a4d8ed9ef0eb02b62
Seung <smo304@nyu.edu>
2014-02-15 12:29:36 +0000

    moving things around

--- ml/make_model.py
+++ ml/make_model.py
@@ -1,23 +1,22 @@
-import data as db
+import dat:a as db
 
 def get_keywords_by_city():
-	cities = {}
+    cities = {}
 
-	# Get every single article
-	articles = db.get_articles()
+    # Get every single article
+    articles = db.get_articles()
 
-	for article in articles():
-		print article["keywords"]
+    for article in articles():
+ 	print article["keywords"]
 
-		word_frequencies = {
-			"murder": 1,
-			"bombing": 3
-		}
+	word_frequencies = {
+	    "murder": 1,
+	    "bombing": 3
+	}
+	# city = get_city_from_position(article["position"])
+    city = "Abu Dhabi, UAE"
 
-		# city = get_city_from_position(article["position"])
-		city = "Abu Dhabi, UAE"
-
-		if (city in cities):
-			cities
-		else:
+    if (city in cities):
+	cities
+    else:
 			
