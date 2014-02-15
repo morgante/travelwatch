de2d0c1c64ad70838043251c5ca6ac807bd8b727
Seung <smo304@nyu.edu>
2014-02-15 13:38:38 +0000

    almost finished makemodel

--- ml/make_model.py
+++ ml/make_model.py
@@ -1,22 +1,38 @@
-import dat:a as db
+import data as db
+import word_frequency as wfr
 
 def get_keywords_by_city():
-    cities = {}
+    cities ={}
 
     # Get every single article
     articles = db.get_articles()
 
     for article in articles():
- 	print article["keywords"]
+	##unsure of the exact notation for this part
+	#############################
+	hl=article["headline"]
+	txt=article["txt"]
+	kw=article["keywords"]
+        city=get_city_from_position(article["position"])
+   	##############################
 
-	word_frequencies = {
-	    "murder": 1,
-	    "bombing": 3
-	}
-	# city = get_city_from_position(article["position"])
-    city = "Abu Dhabi, UAE"
+	##currently using equal weighting
+	wf1=wfr.normalize(wfr.get_frequency(hl))
+	wf2=wfr.normalize(wfr.get_frequency(h2))
+	wf3=wfr.normalize(wfr.get_frequency(h3))
+	wf=wfr.normalize(wfr.add(wfr.add(wf1,wf2),w3))
 
-    if (city in cities):
-	cities
-    else:
+	if city in cities.keys():
+	    cities[city]=wfr.add(cities[city][0],wf)
+	else:
+	    cities[city]=wfr
+    
+    for city in cities:
+	##normalize after all articles have been updated
+	cities[city]=wfr.normalize(cities[city])
+        #have to append a cNum to each city
+
+    return cities 
+
+
 			
