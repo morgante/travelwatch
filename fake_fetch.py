854d332197fba7e02286b4924afce930821b9566
Mahmoud Alismail <mahmoud.alismail@gmail.com>
2014-02-15 19:07:54 +0000

    fetching from NYT & sending to the server

--- fake_fetch.py
+++ fake_fetch.py
@@ -4,27 +4,24 @@ import geocode
 import data as db
 
 data = retrieve.search(
-	    query=None,
-            filters=None, # Input here is a list of dictionaries EVEN FOR ONE ELEMENT: ["web_url"], etc.
+	    query = None,
+            filters={"news_desk": ["U.S.", "World"], "document_type":["article"]},
             begin="20080101",
-            end="20081231",
+            end="20090101",
             page=None,
             pages=1,
             sort=None,
-            fields=["_id", "web_url", "headline", "keywords"],
+            fields=["_id","web_url","lead_paragraph","abstract","headline","keywords","pub_date","word_count","source","document_type","news_desk"],
             highlight=False,
-            facet_field=["source"], # Input here is a list of strings EVEN FOR ONE ELEMENT
+            facet_field=None, # Input here is a list of strings EVEN FOR ONE ELEMENT
             facet_filter=False
 	)
 
+print "\n\n\n\n\n\n"
 
-#data = [{'lead_paragraph': None, 'headline': u'In Fashion, Fame\u2019s Last Bastion', 'snippet': None, 'url': u'http://www.nytimes.com/2014/02/16/fashion/Fashion-Week-celebrities.html', 'date': None, '_id': u'52fead9438f0d826f8460dfb'}]
-#print data
-#print data[0]["url"]
-
-
-for i in data:
-   print i
+print data
+for i in data[0]:
+   #print i
    if i['lead_paragraph'] == None:
 	print "rabish"
 	
@@ -33,11 +30,13 @@ for i in data:
 		"positions": [],
 		"headline": i['headline'],
 		"text": i['lead_paragraph'],
-		"keywords": [],
+		"keywords": i['keywords'],
 		"date": i['date'] 
 	}
    	print "got an article\n"
 	db.insert_article(article)
-   
-
-
+"""
+name: which type is it
+rank:
+value: 
+"""
