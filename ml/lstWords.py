66f0d841fcd7a0af6e3ed30b64ad5db3cc3bb876
Mahmoud Alismail <mahmoud.alismail@gmail.com>
2014-02-15 10:48:17 +0400

    slight fix to list of words

--- ml/lstWords.py
+++ ml/lstWords.py
@@ -1,4 +1,4 @@
-
+# we will need to agree upon the words sometime
 
 def lstofWords():
    lst = ["crime", "abuse", "forgery", "kidnapping", "murder", "prostitution", "theft", "trespass", "danger", "federal", "kill", "warning", "death", "attack", "arrest"]
