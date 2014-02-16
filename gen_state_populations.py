393417d9ac35bcaefccd619c8e3065f0823229b0
Kevin Daniel Aziz <kdaziz7@gmail.com>
2014-02-16 05:56:06 +0000

    New getPop gives population of given state

---
+++ gen_state_populations.py
@@ -0,0 +1,17 @@
+def gen_map():
+  lines = open('usa_crime_data_2008.csv').readlines()
+  mapping = {}
+  for l in lines[1:]: #throw out the title line
+    mapping[l.split(",")[0]] = 0
+  
+  for l in lines[1:]: # ''
+    split = l.split(",")
+    state = split[0]
+    pop = split[2]
+    mapping[state]+=int(pop)
+
+  return mapping
+
+def getPop(state):
+  m = gen_map()
+  return m[state.upper()]
