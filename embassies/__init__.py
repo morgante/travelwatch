a381852fee0dacd7073216fb7824d8e210a4c917
Morgante Pell <morgante.pell@morgante.net>
2014-02-16 05:19:43 +0400

    whoah yo predict from embassy data

--- embassies/__init__.py
+++ embassies/__init__.py
@@ -5,6 +5,34 @@ import data as db
 import canada
 import usa
 
+def get_by_country():
+	data = get_old()
+	countries = {}
+
+	if (data is None):
+		return countries
+
+	for alert in data:
+		if (alert["country"] in countries):
+			country = countries[alert["country"]]
+		else:
+			country = {
+				"code": alert["country"],
+				"alerts": []
+			}
+
+		country["alerts"].append(alert)
+
+		countries[alert["country"]] = country
+
+	for code, country in countries.iteritems():
+		ratings = map(lambda alert: alert["rating"], country["alerts"])
+	 	score = int(float(sum(ratings) / len(ratings)) / 4 * 100)
+
+	 	country["score"] = score
+
+	return countries
+
 def get_old():
 	return db.get_alerts()
 
--- predict.py
+++ predict.py
@@ -1,14 +1,29 @@
 import data as db
 import geo.names as geonames
 import embassies
-from ml import normalize
+
+def by_embassies():
+	countries = []
+
+	for code, country in embassies.get_by_country().iteritems():
+		points = [];
+
+		doc = {
+			"code": code,
+			"name": geonames.get_name_from_code(code),
+			"score": country["score"],
+			"points": points
+		}
+
+		countries.append(doc)
+
+	for country in countries:
+		db.insert_country(country)
+
+	print "Inserted country data"
 
 def main():
-	data = embassies.get_old()
-
-	for alert in data:
-		print (alert["country"], alert["rating"])
-
+	by_embassies()
 
 if __name__ == "__main__":
     main()
