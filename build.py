861ea2c8051516e67d983927ec631b72ddba00df
Morgante Pell <morgante.pell@morgante.net>
2014-02-16 09:06:19 +0400

    freaky frequencies

--- build.py
+++ build.py
@@ -8,7 +8,18 @@ from ml import words
 def get_news():
 	cdata = news.get_by_state()
 
-	print cdata
+	for code, articles in cdata.iteritems():
+		# ratings = map(lambda alert: alert["rating"], country["alerts"])
+	 	# score = int(float(sum(ratings) / len(ratings)) / 4 * 100)
+
+	 	# country["score"] = score
+
+		for article in articles:
+			# print article
+			# print article
+			print words.frequencies(article["snippet"])
+
+	# print cdata
 
 def main():
 	get_news()
