76dcfa902bc225f70a81f8507597d298a0d536b6
Mahmoud Alismail <mahmoud.alismail@gmail.com>
2014-02-16 06:44:06 +0000

    more fixes to reverse

--- geo/reverse.py
+++ geo/reverse.py
@@ -30,17 +30,21 @@ def get_city(point):
 
 def get_state(point):
 
-
- #   (new_place, new_point) = g.reverse(point, exactly_one=True)
- #   splited = new_place.split(',')
+	(new_place, new_point) = g.reverse(change_format(point), exactly_one=True)
+ 	splited = new_place.split(',')
+	print splited[-1]
  #   # address it's different for the states
- #   if splited[-1] == "USA":
-	# remove_address = splited[-2].split(" ")
-	# return remove_address[0]	
- #   else:
-	# # no state
-	# return ""
-   return random.choice(['NY', 'VT', 'CA'])
+	if splited[-1] == "USA" or "America" in splited[-1]:
+		remove_address = splited[-2].split(" ")
+		print remove_address
+		if remove_address[0] ==  "":
+			return splited[-3]
+		return remove_address[0]	
+	else:
+	#no state
+		print "no state"
+		return ""
+   #return random.choice(['NY', 'VT', 'CA'])
 
 
 def get_country(point):
@@ -48,4 +52,4 @@ def get_country(point):
    splited = new_place.split(',')  
    return splited[-1]
   
-#print change_format({'latitude': 123, 'longitude':5678}) 
+print get_state({'latitude': 40.411766, 'longitude': -79.995607}) 
